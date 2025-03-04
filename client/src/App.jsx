850d7427e6cf917030583969313571eb1083d52e
Taggart Smith <taggart.smith@gmail.com>
2025-03-04 08:13:48 -0700

    Created Home Page and fixed styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   return (
     <Router>
       {/* <Navbar /> */}
-      <div className="container mx-auto mt-4">
+      <div className="">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
